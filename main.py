18a462a97bb1f4606f626a143788c68cc481f25b
Evgenia <serogodskaaevgenia@gmail.com>
2024-11-06 21:25:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from fake_math import divide as fake_divide
-from true_math import divide as true_divide
+def fake_divide(first, second):
+    if second == 0:
+        return "Ошибка"
+    else:
+        return first / second
 
-result1 = fake_divide(69, 3)
-result2 = fake_divide(3, 0)
-result3 = true_divide(49, 7)
-result4 = true_divide(15, 0)
-print(result1)
-print(result2)
-print(result3)
-print(result4)
+
+print(fake_divide(69, 3))
+print(fake_divide(3, 0))
